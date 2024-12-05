4ed089de5de3db87ec0face7cbb3de183681e0bb
Hackman <wilfreednouame@gmail.com>
2024-12-06 00:28:01 +0100

    test multiple

--- src/test/java/com/pantxi/calculator/CalculatorTest.java
+++ src/test/java/com/pantxi/calculator/CalculatorTest.java
@@ -4,23 +4,36 @@ import com.pantxi.calculator.Calculator;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 public class CalculatorTest {
 
-    private StringBuilder testString;
+    private Calculator calculator;
 
     // Méthode exécutée avant chaque test
     @BeforeEach
     void setUp() {
-        testString = new StringBuilder("Initial");
-        System.out.println("Setup completed!");
+        calculator= new Calculator(); // Initialiser l'objet Calculat
     }
 
     // Méthode exécutée après chaque test
     @AfterEach
     void tearDown() {
-        testString = null; // Libération des ressources
-        System.out.println("Teardown completed!");
+        calculator = null; // Libération des ressource;
+    }
+    @ParameterizedTest
+    @CsvSource({
+            "0, 1, 1",
+            "1, 2, 3",
+            "-2, 2, 0",
+            "0, 0, 0",
+            "-1, -2, -3",
+
+    })
+void testadd(int a,int b,int expected) {
+        assertEquals(expected, calculator.add(a, b), "Soustraction incorrecte !");
     }
     @Test
 
