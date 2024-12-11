ddf9fa9257179777237288f9ee98a39a22b47a3f
Hackman <wilfreednouame@gmail.com>
2024-12-11 21:01:18 +0100

    ajout de PararmetrizedTest

--- src/test/java/com/pantxi/calculator/CalculatorTest.java
+++ src/test/java/com/pantxi/calculator/CalculatorTest.java
@@ -3,6 +3,8 @@ package com.pantxi.calculator;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -32,6 +34,20 @@ public class CalculatorTest {
         assertEquals(-1,Calculator.add(2,-3), "2+(-3) devrait donner comme resulat 5");*/
 
     }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0, 1, 1",
+            "1, 2, 3",
+            "-2, 2, 0",
+            "0, 0, 0",
+            "-1, -2, -3",
+
+    })
+    void testadd(int a,int b,int expected) {
+        assertEquals(expected, calculator.add(a, b), "addition incorrecte !");
+    }
+
 
     @Test
     void divide_devrait_calculer_la_division_de_deux_entiers(){
